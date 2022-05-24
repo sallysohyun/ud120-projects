8298d8cf34518e68d099061535dbcac69f226f82
sallysohyun <sallysohyun@naver.com>
2022-05-24 17:30:55 +0900

    decision trees done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -26,6 +26,30 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+t_train = time()
+clf = DecisionTreeClassifier(min_samples_split=40)
+
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
+clf.fit(features_train, labels_train)
+print("training time:")
+print(round(time()-t_train, 3), "s")
+
+t_predict = time()
+pred = clf.predict(features_test)
+score = accuracy_score(pred, labels_test)
+print("Accuracy score: ", score)
+print("Predicting Time:", round(time()-t_predict, 3), "s")
+
+
+print("Number of features: ", len(features_train[0]))
+# 10% feature: 3785
+# 1% feature: 379 
+
+
 #########################################################
 
 
